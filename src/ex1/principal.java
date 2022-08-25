a176330be567ece218185b9555572feb937b1e2c
Home <Home@DESKTOP-K1D4CEA>
2022-08-25 15:32:48 -0300

    Primeiro Commit

---
+++ src/ex1/principal.java
@@ -0,0 +1,12 @@
+package ex1;
+
+public class principal {
+
+	public static void main(String[] args) {
+		somacontroller sum = new somacontroller();
+		int soma = 20;
+		int resultado = sum.soma(soma);
+		System.out.println(resultado);
+				
+	}
+}
